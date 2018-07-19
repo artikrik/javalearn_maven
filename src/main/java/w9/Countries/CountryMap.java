0e8c9b10b137898d766083de93dbfa6ea8301fea
artikrik <artikrik@gmail.com>
2018-07-19 13:42:30 +0300

    w8

---
+++ src/main/java/w9/Countries/CountryMap.java
@@ -0,0 +1,32 @@
+package w9.Countries;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class CountryMap {
+    private Map<String, Integer> countryMap;
+
+    CountryMap() {
+        this.countryMap = new HashMap<>();
+    }
+
+    void searchByName(String countryName) {
+        if (countryMap.containsKey(countryName)) {
+            countryMap
+                    .entrySet()
+                    .stream()
+                    .filter(e -> e.getKey().matches(countryName))
+                    .forEach(e -> {
+                        System.out.println(e.getKey() + " : " + e.getValue());
+                    });
+        } else System.out.println("Nothing was found");
+    }
+
+    void print() {
+        System.out.println(countryMap.entrySet());
+    }
+
+    void addCountry(Country country) {
+        countryMap.put(country.getName(), country.getPopulation());
+    }
+}
